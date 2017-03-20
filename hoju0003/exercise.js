df00f4ba774767f65d3b0a71bd090c401b069c46
hoju0003 <hoju0003@mit.edu>
2017-03-20 10:57:02 -0400

    ex-knn submission -Jonathan Ho

---
+++ hoju0003/exercise.js
@@ -0,0 +1,85 @@
+//
+var exercise = {};
+
+// kNN parameters
+roomsMin = 1;
+roomsMax = 10;
+areaMin = 250;
+areaMax = 1700;
+roomsRange = roomsMax-roomsMin;
+areaRange  = areaMax-areaMin;
+
+exercise.measureDistances = function(rooms,area){
+	// ------------------------
+	// YOUR CODE
+	// ------------------------
+
+    data.forEach(function(residence){
+        var deltaRooms= residence.rooms-rooms;
+        deltaRooms=deltaRooms/roomsRange;
+
+        var deltaArea= residence.area-area;
+        deltaArea=deltaArea/areaRange;
+
+        residence.distance= Math.sqrt(deltaRooms*deltaRooms+deltaArea*deltaArea);
+
+    });
+
+
+
+
+
+};
+
+exercise.sortByDistance = function () {
+	// ------------------------
+	// YOUR CODE
+	// ------------------------
+
+    data.sort(function(a,b){
+        return a.distance-b.distance;
+    });
+
+};
+
+exercise.guessType = function(k){
+	// ------------------------
+	// YOUR CODE
+	// ------------------------
+
+	/*
+		// data format you need to return
+		var guess = {type : "house", count : 2};
+		return guess;
+	*/
+    var types ={apartment:0, flat:0, house:0}
+
+    var nn=data.slice(0,k);
+
+    for(var i=0;i<k;i++){
+
+        var nnType =nn[i].type
+        if(nnType==='apartment') types.apartment += 1;
+        if(nnType==='flat') types.flat += 1;
+        if(nnType==='house') types.house += 1;
+
+    }
+
+
+    var guess = {type : false, count : 0};
+
+    for(var j in types){
+        if (types[j]>guess.count){
+            guess.type=j;
+            guess.count=types[j];
+        }
+    }
+
+    return guess;    
+
+
+
+};
+
+// share work
+module.exports = exercise;
