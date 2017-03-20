42c3e06ea937e0eff4ea73a305424cb7394895fa
tmarquart <tmarquart@gmail.com>
2017-03-20 10:53:55 -0400

    tlm

---
+++ tmarquart/exercise.js
@@ -0,0 +1,83 @@
+//
+var exercise = {};
+
+// kNN parameters
+roomsMin = 1;
+roomsMax = 10;
+areaMin = 250;
+areaMax = 1700;
+roomsRange = roomsMax - roomsMin;
+areaRange = areaMax - areaMin;
+
+exercise.measureDistances = function (rooms, area) {
+    // ------------------------
+    // YOUR
+    // ------------------------
+
+    data.forEach(function (residence) {
+        var dRooms = residence.rooms - rooms;
+        dRooms = dRooms / roomsRange;
+
+        var dArea = residence.area - area;
+        dArea = dArea / areaRange;
+
+        residence.distance = Math.sqrt(dRooms * dRooms + dArea * dArea);
+
+    });
+
+    // console.log(data);
+
+};
+
+exercise.sortByDistance = function () {
+    // ------------------------
+    // YOUR CODE
+    // ------------------------
+
+    data.sort(function (a, b) {
+        return (a.distance - b.distance);
+    });
+    console.log(data);
+};
+
+exercise.guessType = function (k) {
+    // ------------------------
+    // YOUR CODE
+    // ------------------------
+
+	/*
+		// data format you need to return
+		var guess = {type : "house", count : 2};
+		return guess;
+	*/
+
+    // var wordarr = {};
+    // for (var i = 0; i < k; i++) {
+    //     wordarr[data[i].type] = (wordarr[data[i].type] || 0) + 1;
+    // }
+
+    var types={apartment:0,flat:0,house:0};
+
+    var nn=data.slice(0,k);
+
+    for(var i=0; i<k; i++){
+        var nnType=nn[i].type;
+        if (nnType==='apartment') types.apartment+=1;
+        if (nnType==='flat') types.flat+=1;
+        if (nnType==='house') types.house+=1;
+    }
+
+    var guess={type:false,count:0};
+    for (var type in types){
+        if (types[type]>guess.count){
+            guess.type=type;
+            guess.count=types[type];
+        }
+    }
+
+    console.log(types);
+    return guess;
+};
+
+// share work
+module.exports = exercise;
